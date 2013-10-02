4e0c1c5f1f7ff2b07a36a09d53faf09da5f1842e
Scott Corgan <scottcorgan@gmail.com>
2013-10-01 18:27:51 -0700

    Add dynamic environment constants.

--- test/divshot.js
+++ test/divshot.js
@@ -1,5 +1,8 @@
+var expect = require('chai').expect;
 var app = require('../lib/divshot');
 
 describe('Divshot cli', function () {
-  
+  it('is in the test environment', function () {
+    expect(process.env.NODE_ENV).to.equal('test');
+  });
 });
