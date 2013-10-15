cc2393d98fb35b0eedcc286c0b3fd4ca603d4029
Collin Miller <collintmiller@gmail.com>
2013-10-15 18:44:34 -0500

    stub server task

---
+++ test/commands/server.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+var stubRequire = require('proxyquire');
+var promote = stubRequire('../../lib/commands/promote', {});
+
+describe('divshot promote [from] [to]', function () {
+  
+});
