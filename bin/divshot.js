fd58713b21284923a97404f2a37f95685d711ca2
Scott Corgan <scottcorgan@gmail.com>
2013-10-28 12:40:04 -0700

    Clean up application start

--- bin/divshot.js
+++ bin/divshot.js
@@ -2,18 +2,10 @@
 
 var path = require('path');
 var mkdirp = require('mkdirp');
-var _ = require('lodash');
 var logger = require('../lib/logger');
 
-process.env.NODE_ENV = (process.env.NODE_ENV) ? process.env.NODE_ENV : 'production';
-
-var env = require('../lib/env');
-var divshotDir = path.join(getUserHome(), '.divshot', 'config');
-
-_.defaults(process.env, env);
-
 // Create our .divshot directory
-mkdirp(divshotDir, function (err) {
+mkdirp(divshotDir(), function (err) {
   if (err) {
     return logger.error('Looks like we don\'t have access to your home directory. Please adjust your permissions before you continue');
   }
@@ -22,6 +14,6 @@ mkdirp(divshotDir, function (err) {
 });
 
 
-function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+function divshotDir() {
+  return path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], '.divshot', 'config');
 }
