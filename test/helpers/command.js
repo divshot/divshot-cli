8fb0f95ebbeeb797ad0e914a6db6f7856fa4760d
Scott Corgan <scottcorgan@gmail.com>
2013-11-06 13:38:47 -0800

    Some more test coverage for loading

--- test/helpers/command.js
+++ test/helpers/command.js
@@ -1,10 +1,9 @@
-var path = require('path');
 var expect = require('expect.js');
 var setup = require('../_setup');
 var app = {program: {commands: [{_name: 'testName'}]}};
 var command = require('../../lib/helpers/command')(app);
 
-describe.only('command helper', function() {
+describe('command helper', function() {
   it('should find a command by the command name', function () {
     expect(command('testName')).to.not.be(undefined);
   });
---
+++ test/helpers/loading.js
@@ -0,0 +1,10 @@
+var expect = require('expect.js');
+var setup = require('../_setup');
+var feedback = require('feedback');
+var loading = require('../../lib/helpers/loading');
+
+describe('loading indicator', function() {
+  it('displays a character at the given intervals', function () {
+    
+  });
+});
