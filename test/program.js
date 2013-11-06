aafe3fc7128d969ebbb2f22ec124ccd8cf780c16
Scott Corgan <scottcorgan@gmail.com>
2013-11-06 11:44:00 -0800

    complete code coverage for User

---
+++ test/program.js
@@ -0,0 +1,8 @@
+var path = require('path');
+var expect = require('expect.js');
+var setup = require('./_setup');
+var program = require('../lib/program.js');
+
+describe.only('#program()', function() {
+  
+});
