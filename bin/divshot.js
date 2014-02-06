0986d3b1cfaf759aa34e278a6f019d326fbcd1a9
Scott Corgan <scott@divshot.com>
2014-02-06 12:55:31 -0800

    Summary of apps

--- bin/divshot.js
+++ bin/divshot.js
@@ -1,27 +1,108 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var mkdirp = require('mkdirp');
-var feedback = require('feedback');
-var chalk = require('chalk');
-var cli = require('../lib/divshot');
-var homdeDir = require('home-dir');
+var divshot = require('../lib');
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2));
 
-// Create our .divshot directory
-mkdirp(path.join(homdeDir(), '.divshot', 'config'), function (err) {
-  if (err) {
-    return feedback.error('Looks like we don\'t have access to your home directory. Please adjust your permissions before you continue');
-  }
-  
-  cli({
-   host: 'https://api.divshot.com',
-    // host: 'http://api.dev.divshot.com:9393'
-  });
+// Run command
+divshot(divshot._command(argv), function () {
+  console.log('ALL DONE!!!!!!!!!!!!!!!');
 });
 
-process.on('uncaughtException', function (err) {
-  feedback.error(err.message);
-  console.log(chalk.red('Stack: ') + err.stack);
+
+// divshot.apps(function (err, apps) {
   
-  process.exit(1);
-});
+// });
+
+// divshot.create('app-name', {/*options here*/}, function (err, apps) {
+  
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var path = require('path');
+// var mkdirp = require('mkdirp');
+// var feedback = require('feedback');
+// var chalk = require('chalk');
+// var cli = require('../lib/divshot');
+// var homdeDir = require('home-dir');
+
+// // Create our .divshot directory
+// mkdirp(path.join(homdeDir(), '.divshot', 'config'), function (err) {
+//   if (err) {
+//     return feedback.error('Looks like we don\'t have access to your home directory. Please adjust your permissions before you continue');
+//   }
+  
+//   cli({
+//    host: 'https://api.divshot.com',
+//     // host: 'http://api.dev.divshot.com:9393'
+//   });
+// });
+
+// process.on('uncaughtException', function (err) {
+//   feedback.error(err.message);
+//   console.log(chalk.red('Stack: ') + err.stack);
+  
+//   process.exit(1);
+// });
+
+
+
+
+
+// var divshot = reqiure('../lib');
+// // var command = something.split(':');
+// var args = [];
+
+// divshot({
+//   command: 'domains',
+//   args: []
+//   task: 'add',
+// }, function () {
+  
+// });
+
+// divshot.domains.add('www.divshot.com', args, function (err) {
+  
+// });
+
+
+
+
+// module.exports = function (options, update, done) {
+//   var appName = options.args.app;
+//   var domainName = options.args[0];
+  
+//   update('My message here', {
+//     type: 'success'
+//   });
+  
+//   done();
+// };
