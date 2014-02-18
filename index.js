36bf435c86c4e33ccfe90fe0bb4d95148e8c8dde
Scott Corgan <scott@divshot.com>
2014-02-18 09:30:10 -0800

    Properly handle --app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var Nash = require('nash');
 var homeDir = require('home-dir');
 var Divshot = require('divshot');
+var format = require('chalk');
 var User = require('./lib/user');
 var Cwd = require('./lib/cwd');
 var commands = require('./lib/commands');
@@ -20,28 +21,34 @@ var description = [
   '  all the power of a modern application platform.'
 ].join('\n');
 
+// Set up CLI
 var cli = Nash.createCli({
   title: title,
   description: description,
   color: 'yellow',
+  
   api: Divshot.createClient({
     token: user.get('token'),
     host: API_HOST
   }),
   user: user,
   cwd: cwd,
-  config: cwd.getConfig(),
   errors: errors
 });
 
 // Flags
 cli.flag('-a', '--app')
   .description('override the directory\'s app name')
-  .handler(function (appName) {});
+  .handler(function (appName) {
+    cwd.overrideAppName(appName);
+  });
 
 cli.flag('-t', '--token')
   .description('override your current user authentication token')
-  .handler(function (token) {});
+  .handler(function (token) {
+    cli.user.attributes.token = token;
+    cli.api.setToken(token);
+  });
 
 // Helpers
 cli.helper('authenticate', function (command, done) {
@@ -49,6 +56,17 @@ cli.helper('authenticate', function (command, done) {
   done();
 });
 
+cli.catchAll(function (type, attemptedCommand) {
+  cli.log();
+  cli.log(format.bold('"' + attemptedCommand + '"') + ' is not a Divshot ' + type + '.');
+  cli.log('Please use ' + format.bold('"divshot help"') + ' for a list of Divshot commands.');
+});
+
+// 
+// TODO: make a secret(true) method for commands to hide from help
+// list
+
+// Add commands
 commands.connect(cli);
 
 module.exports = cli;
