fe8adacc7dc0eae61c486aa7c18862b8b6864644
Scott Corgan <scott@divshot.com>
2014-02-21 19:39:03 -0800

    Fix spelling error

--- bin/divshot.js
+++ bin/divshot.js
@@ -1,29 +1,4 @@
 #!/usr/bin/env node
 
 var cli = require('../index.js');
-
 cli.run(process.argv);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var divshot = require('../lib');
-// var minimist = require('minimist');
-// var argv = minimist(process.argv.slice(2));
-
-// // Run command
-// divshot(divshot._command(argv));
